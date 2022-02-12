37e826f8cbd12aa61e1aafd451c8104dd14f874c
LeviMaor <levimaor@gmail.com>
2022-02-12 21:32:23 +0200

    MovementPart1

--- Final Game/Assets/Scripts/PlayerMovement.cs
+++ Final Game/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D body;
     [SerializeField] private float speed;
-
+    private bool grounded;
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -14,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput* speed, body.velocity.y);
-
+        // Flip The Player When Moving Left-Right
         if(horizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -25,10 +27,29 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1 ,1);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            Jump();
         }
 
+        // Set Animator Parameters
+        anim.SetBool("grounded", grounded);
+        anim.SetBool("run", horizontalInput != 0);
+
     }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
 }
