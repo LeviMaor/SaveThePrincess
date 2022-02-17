f62261b25ad574dc083a0d8a9542309521d6032f
LeviMaor <levimaor@gmail.com>
2022-02-17 18:04:10 +0200

    IframesDone

--- Final Game/Assets/Scripts/Health/Health.cs
+++ Final Game/Assets/Scripts/Health/Health.cs
@@ -1,13 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spritRenderer;
+    
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spritRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage (float _damage)
@@ -15,18 +27,37 @@ public class Health : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
         if(currentHealth > 0)
         {
-
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
-
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
         }
 
     }
 
-    private void Update()
+    public void AddHealth(float _value)
     {
-        if (Input.GetKeyDown(KeyCode.E))
-            TakeDamage(1);
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spritRenderer.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration / (numberOfFlashes * 2));
+            spritRenderer.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
